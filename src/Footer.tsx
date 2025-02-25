0055c3f85e8982f56071218ff7c234d790c66a83
Merrywindl <whasup42@hotmail.com>
2025-02-25 16:19:58 -0600

    fixed typo

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer style={{ marginTop: '20px', textAlign: 'center', padding: '10px 0', backgroundColor: '#f8f9fa', width: '100%' }}>
-      <p>&copy; {new Date().getFullYear()} Landover Labs 2025 All rights reserved. V1.2</p>
+      <p>&copy; {new Date().getFullYear()} Landover Labs 2025 All rights reserved. V1.22</p>
     </footer>
   );
 };
